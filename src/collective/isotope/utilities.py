d1f6cd14e1167fc86b433d598dc72c930600e6df
cewing <cris@crisewing.com>
2015-09-23 18:12:57 -0700

    register one utility for converting the Creator column to a user's fullname

---
+++ src/collective/isotope/utilities.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from plone import api
+
+
+class CreatorConverter(object):
+
+    def convert(self, userid):
+        """return the fullname of the user"""
+        membership = api.portal.get_tool('portal_membership')
+        user_info = membership.getMemberInfo(userid)
+        converted = ''
+        if user_info:
+            converted = user_info['fullname'] or user_info['username']
+        return converted or userid

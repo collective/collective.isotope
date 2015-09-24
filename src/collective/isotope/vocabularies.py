924e0af821bff87c876dae17cb2ebd1bb3f982cc
cewing <cris@crisewing.com>
2015-09-23 17:51:12 -0700

    adjust vocabs to match new usages with the data grid field

--- src/collective/isotope/vocabularies.py
+++ src/collective/isotope/vocabularies.py
@@ -2,13 +2,13 @@
 """Vocabularies to use with collective.isotope"""
 from plone import api
 from plone.registry.interfaces import IRegistry
+from Products.CMFPlone.utils import safe_unicode
 from zope.component import getUtility
 from zope.schema.vocabulary import SimpleVocabulary
 
 
-FILTER_COLUMN_BLACKLIST = set([
+COLUMN_BLACKLIST = set([
     'total_comments',
-    'Title',
     'commentators',
     'exclude_from_nav',
     'id',
@@ -18,10 +18,8 @@ FILTER_COLUMN_BLACKLIST = set([
     'is_folderish',
     'sync_uid',
     'getId',
-    'ExpirationDate',
     'getRemoteUrl',
     'location',
-    'EffectiveDate',
     'portal_type',
     'expires',
     'last_comment_date',
@@ -35,40 +33,17 @@ FILTER_COLUMN_BLACKLIST = set([
     'in_response_to',
 ])
 
-SORT_COLUMN_BLACKLIST = set([
-    'total_comments',
-    'commentators',
-    'exclude_from_nav',
-    'id',
-    'cmf_uid',
-    'Description',
-    'listCreators',
-    'is_folderish',
-    'sync_uid',
-    'getId',
-    'ExpirationDate',
-    'review_state',
-    'getRemoteUrl',
-    'location',
-    'portal_type',
-    'expires',
-    'last_comment_date',
-    'UID',
-    'effective',
-    'Creator',
-    'getIcon',
-    'created',
-    'modified',
-    'meta_type',
-    'in_response_to',
-    'Subject'
-])
-
 
 class MetadataColumnVocabulary(object):
-    """parameterized vocabulary providing items from catalog metadata columns"""
+    """Build terms from the list of catalog metadata columns
 
-    def __init__(self, blacklist):
+    The vocabulary can optionally take a 'blacklist' which will filter
+    the terms available.
+    """
+
+    def __init__(self, blacklist=None):
+        if blacklist is None:
+            blacklist = set([])
         self.blacklist = blacklist
 
     def __call__(self, context):
@@ -78,28 +53,33 @@ class MetadataColumnVocabulary(object):
         items = sorted([(name.decode('utf8'), name) for name in possible])
         return SimpleVocabulary.fromItems(items)
 
-FilterColumnVocabularyFactory = MetadataColumnVocabulary(FILTER_COLUMN_BLACKLIST)
-SortColumnVocabularyFactory = MetadataColumnVocabulary(SORT_COLUMN_BLACKLIST)
+FriendlyColumnVocabularyFactory = MetadataColumnVocabulary(COLUMN_BLACKLIST)
 
 
-class RegistryFieldVocabulary(object):
-    """parameterized vocabulary providing items from a field in the configuration registry"""
+class FilterFieldVocabulary(object):
+    """Build terms from a datagrid field providing the filter schema"""
 
     def __init__(self, registry_key):
         self.registry_key = registry_key
 
     def __call__(self, context):
         registry = getUtility(IRegistry)
-        values = registry.get(self.registry_key, [])
+        rows = registry.get(self.registry_key, [])
         terms = []
-        for value in values:
-            terms.append(SimpleVocabulary.createTerm(value, str(value)))
+        for row in rows:
+            value = row.get('column_name')
+            title = row.get('label')
+            if not title:
+                title = value
+            terms.append(
+                SimpleVocabulary.createTerm(value, value, safe_unicode(title))
+            )
         return SimpleVocabulary(terms)
 
 
-FiltersVocabularyFactory = RegistryFieldVocabulary(
-    'collective.isotope.interfaces.ICollectiveIsotopeSettings.filter_whitelist'
+FiltersVocabularyFactory = FilterFieldVocabulary(
+    'collective.isotope.interfaces.ICollectiveIsotopeSettings.available_filters'
 )
-SortsVocabularyFactory = RegistryFieldVocabulary(
-    'collective.isotope.interfaces.ICollectiveIsotopeSettings.sorting_whitelist'
+SortsVocabularyFactory = FilterFieldVocabulary(
+    'collective.isotope.interfaces.ICollectiveIsotopeSettings.available_sorts'
 )

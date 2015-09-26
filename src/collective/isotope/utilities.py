31d0f94ed354f0150c0851e50b7a42c3307cb673
cewing <cris@crisewing.com>
2015-09-25 21:10:57 -0700

    Change of plans, make converter utilitites simple callables.  That way we can simply call them, and not have to instantiate them.

--- src/collective/isotope/utilities.py
+++ src/collective/isotope/utilities.py
@@ -1,14 +1,11 @@
 # -*- coding: utf-8 -*-
 from plone import api
 
-
-class CreatorConverter(object):
-
-    def convert(self, userid):
-        """return the fullname of the user"""
-        membership = api.portal.get_tool('portal_membership')
-        user_info = membership.getMemberInfo(userid)
-        converted = ''
-        if user_info:
-            converted = user_info['fullname'] or user_info['username']
-        return converted or userid
+def creator_converter(userid):
+    """return the fullname of the user"""
+    membership = api.portal.get_tool('portal_membership')
+    user_info = membership.getMemberInfo(userid)
+    converted = ''
+    if user_info:
+        converted = user_info['fullname'] or user_info['username']
+    return converted or userid

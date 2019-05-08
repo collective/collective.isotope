dc8d9f64e69fad939484352ced01d28c10477fce
Jesse Snyder <jsnyder@wesleyan.edu>
2019-05-08 09:44:58 -0700

    Fix tests

--- src/collective/isotope/tests/test_setup.py
+++ src/collective/isotope/tests/test_setup.py
@@ -4,8 +4,11 @@ from collective.isotope.testing import (
     COLLECTIVE_ISOTOPE_INTEGRATION_TESTING,  # noqa
 )
 from plone import api
-
-import unittest2 as unittest
+from Products.CMFPlone.utils import get_installer
+try:
+    import unittest
+except ImportError:
+    import unittest2 as unittest
 
 
 class TestSetup(unittest.TestCase):
@@ -16,12 +19,12 @@ class TestSetup(unittest.TestCase):
     def setUp(self):
         """Custom shared utility setup for tests."""
         self.portal = self.layer['portal']
-        self.installer = api.portal.get_tool('portal_quickinstaller')
+        self.installer = get_installer(self.portal)
 
     def test_product_installed(self):
         """Test if collective.isotope is installed with portal_quickinstaller."""
         self.assertTrue(
-            self.installer.isProductInstalled('collective.isotope')
+            self.installer.is_product_installed('collective.isotope')
         )
 
     def test_browserlayer(self):
@@ -38,13 +41,13 @@ class TestUninstall(unittest.TestCase):
 
     def setUp(self):
         self.portal = self.layer['portal']
-        self.installer = api.portal.get_tool('portal_quickinstaller')
-        self.installer.uninstallProducts(['collective.isotope'])
+        self.installer = get_installer(self.portal)
+        self.installer.uninstall_product('collective.isotope')
 
     def test_product_uninstalled(self):
         """Test if collective.isotope is cleanly uninstalled."""
         self.assertFalse(
-            self.installer.isProductInstalled('collective.isotope')
+            self.installer.is_product_installed('collective.isotope')
         )
 
     def test_browserlayer_removed(self):
